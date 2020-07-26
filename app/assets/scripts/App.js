8e4eafb2f8e8a0ce91787614fdd0ca5787474c92
topgreens <topgreens@mail.com>
2020-07-26 23:53:30 +0200

    working on the header and contact overlay modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,6 +4,21 @@ import RevealOnScroll from './modules/RevealOnScroll'
 
 new RevealOnScroll(document.querySelectorAll(".about-wrapper"), 75)
 new RevealOnScroll(document.querySelectorAll(".post-wrapper"), 60)
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if (typeof modal == "undefined") {
+            import(/*webpackChunkName: "modal" */'./modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log("There was a problem."))
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
 
 if (module.hot) {
     module.hot.accept()
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,54 @@
+class Modal {
+    constructor() {
+        this.injectHTML()
+        this.modal = document.querySelector(".modal")
+        this.closeIcon = document.querySelector(".modal__close")
+        this.events()
+    }
+
+    events() {
+        // listen for close click
+        this.closeIcon.addEventListener("click", () => this.closeTheModal())
+
+        // pushes any key
+        document.addEventListener("keyup", e => this.keyPressHandler(e))
+    }
+
+    keyPressHandler(e) {
+        if (e.keyCode == 27) {
+            this.closeTheModal()
+        }
+    }
+
+    openTheModal() {
+        this.modal.classList.add("modal--is-visible")
+    }
+
+    closeTheModal() {
+        this.modal.classList.remove("modal--is-visible")
+    }
+
+    injectHTML() {
+        document.body.insertAdjacentHTML('beforeend', `
+    <div class="modal">
+		<div class="modal__inner">
+			<h2 class="section-title section-title--blue section-title--less-margin"><img
+					src="assets/images/icon/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
+			<div class="wrapper wrapper--narrow">
+				<p class="modal__description">I will have a new Post in place soon. Until then, connect with me
+					on any of the platforms below!</p>
+			</div>
+
+			<div class="social-icons">
+				<a href="#" class="social-icons__icon"><img src="assets/images/icon/facebook.svg" alt="Facebook"></a>
+				<a href="#" class="social-icons__icon"><img src="assets/images/icon/twitter.svg" alt="Twitter"></a>
+				<a href="#" class="social-icons__icon"><img src="assets/images/icon/instagram.svg" alt="Instagram"></a>
+			</div>
+		</div>
+		<div class="modal__close">X</div>
+	</div>
+    `)
+    }
+}
+
+export default Modal;
