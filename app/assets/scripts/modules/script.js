12b0cf2443bf3d068e0194b8a8a06e87a322ca29
topgreens <topgreens@mail.com>
2020-07-26 17:15:26 +0200

    modified the footer font and set the css path in the header

--- app/assets/scripts/modules/script.js
+++ app/assets/scripts/modules/script.js
@@ -21,19 +21,19 @@ for (var i = 0; themeDots.length > i; i++) {
 
 function setTheme(mode) {
 	if (mode == 'light') {
-		document.getElementById('theme-style').href = '../scripts/modules/default.css'
+		document.getElementById('theme-style').src = 'default.css'
 	}
 
 	if (mode == 'blue') {
-		document.getElementById('theme-style').href = '../scripts/modules/blue.css'
+		document.getElementById('theme-style').src = 'blue.css'
 	}
 
 	if (mode == 'green') {
-		document.getElementById('theme-style').href = '../scripts/modules/green.css'
+		document.getElementById('theme-style').src = 'green.css'
 	}
 
 	if (mode == 'purple') {
-		document.getElementById('theme-style').href = '../scripts/modules/purple.css'
+		document.getElementById('theme-style').src = 'purple.css'
 	}
 
 	localStorage.setItem('theme', mode)
