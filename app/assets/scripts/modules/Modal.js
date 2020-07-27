16ff351265c35292e75b49ef156164fe5a5bddb1
topgreens <topgreens@mail.com>
2020-07-27 03:06:38 +0200

    added a lightbox overlay

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -41,7 +41,8 @@ class Modal {
 
 			<div class="social-icons">
 				<a href="#" class="social-icons__icon"><img src="assets/images/icon/facebook.svg" alt="Facebook"></a>
-				<a href="#" class="social-icons__icon"><img src="assets/images/icon/twitter.svg" alt="Twitter"></a>
+                <a href="#" class="social-icons__icon"><img src="assets/images/icon/twitter.svg" alt="Twitter"></a>
+                <a href="#" class="social-icons__icon"><img src="assets/images/icon/linkedin.svg" alt="LinkedIn"></a>
 				<a href="#" class="social-icons__icon"><img src="assets/images/icon/instagram.svg" alt="Instagram"></a>
 			</div>
 		</div>
